ba532126e831aa58d6c35e69902110edab99b5fb
DevEunice <obebeeunice@gmail.com>
2022-10-27 17:19:39 +0100

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,34 @@
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Product from './pages/Product';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to="/">amazona</Link>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <LinkContainer to ="/">
+            <Navbar.Brand>amazona</Navbar.Brand>
+          </LinkContainer>
+        </Container>
+      </Navbar>
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product/:slug" element={<Product />} />
         </Routes>
-        
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'>All rights reserved</div>
+      </footer>
     </div>
     </BrowserRouter>
   );
